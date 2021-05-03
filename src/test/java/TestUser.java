9ffa6fdf1ad8d4dd4a3071284215da1e1638149c
Ponram2k <82449745+Ponram2k@users.noreply.github.com>
2021-05-03 12:29:59 +0530

    Update TestUser.java

--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -1,111 +1,45 @@
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import com.stock.Product;
-import com.stock.ProductManager;
+import com.stock.User;
 
-public class TestProduct {
+public class TestUser {
 
-	@Before
-	public void setUp() throws Exception {
-		
-		Product product1 = new Product();
-		product1.itemCode = 1;
-		product1.brandName = "Nokia";
-		product1.itemCategory = "Mobile";
-		product1.itemName = "9s";
-		product1.rate = 9000;
-		product1.quantity = 10;
-		product1.amount = product1.rate * product1.quantity;
-
-		Product product2 = new Product();
-		product2.itemCode = 2;
-		product2.brandName = "Iphone";
-		product2.itemCategory = "Mobile";
-		product2.itemName = "SE2020";
-		product2.rate = 40000;
-		product2.quantity = 10;
-		product2.amount = product2.rate * product2.quantity;
-
-		Product product3 = new Product();
-		product3.itemCode = 3;
-		product3.brandName = "Asus";
-		product3.itemCategory = "Laptop";
-		product3.itemName = "X509UA";
-		product3.rate = 30000;
-		product3.quantity = 10;
-		product3.amount = product3.rate * product3.quantity;
-
-		Product product4 = new Product();
-		product4.itemCode = 4;
-		product4.brandName = "Boat";
-		product4.itemCategory = "Heatset";
-		product4.itemName = "Airdot935";
-		product4.rate = 2000;
-		product4.quantity = 10;
-		product4.amount = product4.rate * product4.quantity;
-
-		// Add 4 different product in the ArrayList
-		ProductManager.addStock(product1);
-		ProductManager.addStock(product2);
-		ProductManager.addStock(product3);
-		ProductManager.addStock(product4);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		ProductManager.stockDetails.clear();
+	@Test
+	public void usernameValidationTest() {
+		boolean existsLogin = User.logIn("Adm","Admin1@232");
+		assertEquals(false, existsLogin);
 	}
 	
 	@Test
-	public void testAddStock() {
-
-		Product checkProduct = ProductManager.stockDetails.get(0);
-		assertEquals("Nokia", checkProduct.brandName);
-	}
-
-	@Test
-	public void testDisplayStock() {
-		
-		ProductManager.displayStock();
+	public void passwordValidationTest() {
+		boolean existsLogin = User.logIn("Admin","Admin1");
+		assertEquals(false, existsLogin);
 	}
 	
 	@Test
-	public void testGetStock() {
-		
-		ArrayList<Product> checkList = new ArrayList<Product>();
-		checkList = ProductManager.getStock();
-		
-		// Get the particular product from the 0 index
-		Product checkProduct = checkList.get(0); 
-		
-		// Check the product is correctly added in ArrayList
-		assertEquals(1, checkProduct.itemCode);
+	public void bothUserNameAndPasswordCorrect() {
+		boolean existsLogin = User.logIn("Admin","Admin@1234");
+		assertEquals(true, existsLogin);
 	}
 	
 	@Test
-	public void testFilterStock() {
-		
-		ProductManager.filterStockByCategory("laptop");
+	public void bothUserNameAndPasswordWrong() {
+		boolean existsLogin = User.logIn("Admin1","Admin1@232");
+		assertEquals(false, existsLogin);
 	}
 	
 	@Test
-	public void testgetFilterStock() {
-
-		ArrayList<Product> fliterCheckList = new ArrayList<Product>();
-		
-		// store the filtered list in checkList from productDetails ArrayList
-		fliterCheckList = ProductManager.getFilterStockByCategory("laptop");
-		
-		// get particular product from checkList store it in checkProduct
-		Product checkProduct = fliterCheckList.get(0);
-		
-		// check the given category is correctly added in checkProduct
-		assertEquals("Laptop", checkProduct.itemCategory);
+	public void usernameCorrectAndPasswordWrong() {
+		boolean existsLogin = User.logIn("Admin","Admin1@232");
+		assertEquals(false, existsLogin);
 	}
+	
+	@Test
+	public void usernameWrongAndPasswordCorrect() {
+		boolean existsLogin = User.logIn("Admin1","Admin1@232");
+		assertEquals(false, existsLogin);
+	}
+	
 }
